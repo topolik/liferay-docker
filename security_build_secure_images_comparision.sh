b361d421fc76cb66e3d03c7341dcaa316fa476d4
Tomáš Polešovský <tomas.polesovsky@liferay.com>
2023-12-08 18:15:18 +0100

    security scan of ubi images

--- security_build_secure_images_comparision.sh
+++ security_build_secure_images_comparision.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-UBUNTU_PRO_TOKEN=$(op item get "Ubuntu Pro Token" --fields=password)
-IMAGE="liferay/portal:7.4.3.103-ga103-d5.0.54-20231117111559"
+IMAGE="liferay/portal:7.4.3.103-ga103"
 
 SCRIPTPATH="$(cd "$(dirname "$0")" >/dev/null 2>&1; pwd -P)"
 CVE_DIR=temp-cve-$(date +\%Y-\%m-\%d)
@@ -8,9 +7,12 @@ CVE_DIR=temp-cve-$(date +\%Y-\%m-\%d)
 mkdir -p /tmp/trivy-cache "$CVE_DIR"
 
 echo "♾"
-echo "♾ Pulling $IMAGE ..."
+echo "♾ Building $IMAGE ..."
 echo "♾"
-docker pull "$IMAGE"
+LIFERAY_DOCKER_DEVELOPER_MODE=true ./build_base_image.sh 
+LIFERAY_DOCKER_DEVELOPER_MODE=true ./build_jdk11_image.sh 
+export LIFERAY_DOCKER_RELEASE_FILE_URL=http://releases-cdn.liferay.com/portal/7.4.3.103-ga103/liferay-ce-portal-tomcat-7.4.3.103-ga103-20231116132758925.7z
+LIFERAY_DOCKER_DEVELOPER_MODE=true ./build_bundle_image.sh 
 
 
 ####
@@ -26,8 +28,9 @@ FROM $IMAGE
 
 USER root
 
-RUN apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y && apt autoclean -y
-
+RUN yum update -y --disableplugin=subscription-manager && \
+	yum upgrade -y --disableplugin=subscription-manager && \
+	yum clean all
 EOF
 )
 
@@ -37,57 +40,6 @@ echo "♾"
 
 docker run --rm --volume /tmp/trivy-cache:/root/.cache/ --volume /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image --scanners vuln --timeout 15m "$IMAGE-updated" --format json > /tmp/trivy-ubuntu-updated.json
 
-
-####
-#### Ubuntu PRO image with updated dependencies
-####
-echo "♾"
-echo "♾ Building Ubuntu PRO $IMAGE-esm with updated dependencies..."
-echo "♾"
-
-docker build --tag "$IMAGE-esm" - < <(cat <<EOF
-FROM $IMAGE
-
-USER root
-
-# Insecure, but for simplicity sake, use only for local images!
-RUN apt update && apt install ubuntu-advantage-tools -y && pro attach $UBUNTU_PRO_TOKEN
-
-RUN apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y && apt autoclean -y
-
-EOF
-)
-
-echo "♾"
-echo "♾ Computing vulnerabilities of $IMAGE-esm"
-echo "♾"
-
-docker run --rm --volume /tmp/trivy-cache:/root/.cache/ --volume /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image --scanners vuln --timeout 15m "$IMAGE-esm" --format json > /tmp/trivy-ubuntu-esm.json
-
-
-
-#
-# Remove false positives with Ubuntu Pro's `pro fix CVE` command
-#
-# That means, fetch each reported CVE and update the state into $CVE_DIR/$CVE.txt
-
-echo "♾"
-echo "♾ Downloading vulnerabilities descriptions ..."
-echo "♾"
-
-
-cat /tmp/trivy-ubuntu-esm.json | jq '.Results[] | select(.Class == "os-pkgs") | .Vulnerabilities[] | .VulnerabilityID' -r  | sort -u > $CVE_DIR/reported-cves.txt
-
-cat <<"EOF" >"$CVE_DIR/check-reported.sh"
-#!/bin/bash
-cat "/tmp/cve/reported-cves.txt" | while read CVE; do test -f "/tmp/cve/$CVE.txt" || pro fix "$CVE" > "/tmp/cve/$CVE.txt"; done
-EOF
-
-chmod a+x "$CVE_DIR/check-reported.sh"
-docker run --rm \
-    --entrypoint="/tmp/cve/check-reported.sh" \
-    -v "${SCRIPTPATH}/${CVE_DIR}":/tmp/cve \
-    "$IMAGE-esm"
 
 ####
 #### Default image with removed dependencies
@@ -102,12 +54,33 @@ FROM $IMAGE
 
 USER root
 
-RUN apt remove --yes bc ffmpeg file fonts-dejavu fonts-droid-fallback ghostscript google-perftools imagemagick gifsicle libtcnative-1
+RUN yum remove -y \
+		# breaks yum: bash\
+		ca-certificates\
+		curl\
+		jq\
+		less\
+		# not installed: not found: libnss3\
+		# not installed: not found: telnet\
+		# not installed: not found: tini\
+		# not installed: not found: tree\
+		unzip
 
-# A good replacement?
-# RUN apt update && apt install graphicsmagick -y
+RUN yum remove -y \
+		bc \
+		# not installed:  ffmpeg \
+		file \
+		# not installed: fonts-dejavu \
+		# not installed:  fonts-droid-fallback \
+		ghostscript \
+		# not installed: google-perftools \
+		# not installed: imagemagick \
+		# not installed: gifsicle \
+		# not installed: libtcnative-1 \
 
-RUN apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y && apt autoclean -y
+RUN yum update -y --disableplugin=subscription-manager && \
+	yum upgrade -y --disableplugin=subscription-manager && \
+	yum clean all
 
 EOF
 )
@@ -119,61 +92,6 @@ echo "♾"
 
 docker run --rm --volume /tmp/trivy-cache:/root/.cache/ --volume /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image --scanners vuln --timeout 15m "$IMAGE-manual-remove" --format json > /tmp/trivy-ubuntu-manual-remove.json
 
-
-####
-#### Ubuntu PRO with removed dependencies
-####
-
-echo "♾"
-echo "♾ Building Ubuntu PRO $IMAGE-esm-manual-remove with removed dependencies..."
-echo "♾"
-
-docker build --tag "$IMAGE-esm-manual-remove" - < <(cat <<EOF
-FROM $IMAGE
-
-USER root
-
-# Insecure, but for simplicity sake, use only for local images!
-RUN apt update && apt install ubuntu-advantage-tools -y && pro attach $UBUNTU_PRO_TOKEN
-
-RUN apt remove --yes bc ffmpeg file fonts-dejavu fonts-droid-fallback ghostscript google-perftools imagemagick gifsicle libtcnative-1
-
-# A good replacement?
-# RUN apt update && apt install graphicsmagick -y
-
-RUN apt update && apt upgrade -y && apt dist-upgrade -y && apt autoremove -y && apt autoclean -y
-
-EOF
-)
-
-echo "♾"
-echo "♾ Computing vulnerabilities of $IMAGE-esm-manual-remove"
-echo "♾"
-
-docker run --rm --volume /tmp/trivy-cache:/root/.cache/ --volume /var/run/docker.sock:/var/run/docker.sock aquasec/trivy image --scanners vuln --timeout 15m "$IMAGE-esm-manual-remove" --format json > /tmp/trivy-ubuntu-esm-manual-remove.json
-
-
-#
-# Remove false positives with Ubuntu Pro's `pro fix CVE` command
-#
-# That means, fetch each reported CVE and update the state into $CVE_DIR/$CVE.txt
-
-echo "♾"
-echo "♾ Downloading vulnerabilities descriptions ..."
-echo "♾"
-
-cat /tmp/trivy-ubuntu-esm-manual-remove.json | jq '.Results[] | select(.Class == "os-pkgs") | .Vulnerabilities[] | .VulnerabilityID' -r  | sort -u > "$CVE_DIR/reported-cves.txt"
-
-cat <<"EOF" >"$CVE_DIR/check-reported.sh"
-#!/bin/bash
-cat /tmp/cve/reported-cves.txt | while read CVE; do test -f "/tmp/cve/$CVE.txt" || pro fix "$CVE" > "/tmp/cve/$CVE.txt"; done
-EOF
-
-chmod a+x "$CVE_DIR/check-reported.sh"
-docker run --rm \
-    --entrypoint="/tmp/cve/check-reported.sh" \
-    -v "${SCRIPTPATH}/${CVE_DIR}":/tmp/cve \
-    "$IMAGE-esm"
 
 
 ################
@@ -194,29 +112,6 @@ echo "TOTAL: $(cat /tmp/trivy-temp.txt | wc -l)" && rm /tmp/trivy-temp.txt
 echo "---------------------------------------"
 echo ""
 
-
-echo "Ubuntu PRO with updated dependencies"
-echo "========================================"
-
-cat "$CVE_DIR"/CVE-* | grep "CVE-.* is" > "$CVE_DIR/reported-cves-checked.txt"
-
-cat /tmp/trivy-ubuntu-esm.json | jq '.Results[] | select(.Class == "os-pkgs") | .Vulnerabilities[] | "\(.VulnerabilityID) \(.Severity) \(.PkgName) \(.CVSS.nvd.V3Score)"' -r | sort -u | while read line; do
-    cve=$(echo "$line" | cut -d ' ' -f 1)
-    if grep "$cve is resolved" "$CVE_DIR/reported-cves-checked.txt" > /dev/null; then
-        continue;
-    fi
-    echo "$line"
-done | grep -v -E '[0123456]\.[0-9]$'| grep -v -E -i 'low|negligible' | tee /tmp/trivy-temp.txt | cut -d ' ' -f1 | sort -u | while read CVE; do
-    cat "$CVE_DIR/$CVE.txt" | grep 'affected\|not resolved' | grep -v source | sed 's/resolved./resolved.#/' | tr '\n' ' ' | tr '#' '\n' | sed -re 's/(^[[:space:]]+|[[:space:]]+$)//g' 
-done
-
-cat /tmp/trivy-temp.txt | sed -re 's/(^[[:space:]]+|[[:space:]]+$)//g' 
-echo ""
-echo "TOTAL: $(cat /tmp/trivy-temp.txt | wc -l)" && rm /tmp/trivy-temp.txt
-echo "---------------------------------------"
-echo ""
-
-
 echo "Default image with removed dependencies"
 echo "========================================"
 cat /tmp/trivy-ubuntu-manual-remove.json | \
@@ -229,27 +124,3 @@ echo ""
 echo "TOTAL: $(cat /tmp/trivy-temp.txt | wc -l)" && rm /tmp/trivy-temp.txt
 echo "---------------------------------------"
 echo ""
-
-
-echo "Ubuntu PRO with removed dependencies"
-echo "========================================"
-
-cat "$CVE_DIR"/CVE-* | grep "CVE-.* is" > "$CVE_DIR/reported-cves-checked.txt"
-
-cat /tmp/trivy-ubuntu-esm-manual-remove.json | jq '.Results[] | select(.Class == "os-pkgs") | .Vulnerabilities[] | "\(.VulnerabilityID) \(.Severity) \(.PkgName) \(.CVSS.nvd.V3Score)"' -r | sort -u | while read line; do
-    cve=$(echo "$line" | cut -d ' ' -f 1)
-    if grep "$cve is resolved" "$CVE_DIR/reported-cves-checked.txt" > /dev/null; then
-        continue;
-    fi
-    echo "$line"
-done | grep -v -E '[0123456]\.[0-9]$'| grep -v -E -i 'low|negligible' | tee /tmp/trivy-temp.txt | cut -d ' ' -f1 | sort -u | while read CVE; do
-    cat "$CVE_DIR/$CVE.txt" | grep 'affected\|not resolved' | grep -v source | sed 's/resolved./resolved.#/' | tr '\n' ' ' | tr '#' '\n' | sed -re 's/(^[[:space:]]+|[[:space:]]+$)//g' 
-done
-
-
-cat /tmp/trivy-temp.txt | sed -re 's/(^[[:space:]]+|[[:space:]]+$)//g' 
-echo ""
-echo "TOTAL: $(cat /tmp/trivy-temp.txt | wc -l)" && rm /tmp/trivy-temp.txt
-echo "---------------------------------------"
-echo ""
-
